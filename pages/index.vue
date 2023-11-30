53ce5a54d6995d0df6e9b199410d7a2dca8a69fe
Hugo <hmoralesg@icloud.com>
2023-11-30 00:29:17 -0300

    feat: added vuetify

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-100 h-100 flex-column d-flex align-center justify-center">
+    <h1 class="py-4">Home page</h1>
+    <v-btn class="py-4" prepend-icon="$vuetify" @click="hi">
+      Click me
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+const hi = () => console.log('hi')
+</script>
