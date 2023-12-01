70c416cd86db198b9ac7e240c3d1d02d3d6dba8a
Hugo <hmoralesg@icloud.com>
2023-12-01 02:31:40 -0300

    feat: installed eslint basic setup with nuxt styleguide

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,13 +8,16 @@ export default defineNuxtConfig({
     ],
   },
     modules: [
+    // Vuetify vite module
+    // https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
         config.plugins.push(vuetify({ autoImport: true }))
       })
     },
-    //...
+    // Eslint module
+    '@nuxtjs/eslint-module',
   ],
   vite: {
     vue: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="w-100 h-100 flex-column d-flex align-center justify-center">
-    <h1 class="py-4">Home page</h1>
-    <v-btn class="py-4" prepend-icon="$vuetify" @click="hi">
+    <h1 class="py-4">
+      Home page
+    </h1>
+    <v-btn
+      class="py-4"
+      prepend-icon="$vuetify"
+      @click="hi"
+    >
       Click me
     </v-btn>
   </div>
