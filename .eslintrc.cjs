11b3dcd38c4421d380c2845d0bdb686e25287c69
Hugo <hmoralesg@icloud.com>
2023-12-04 22:07:23 -0300

    feat: eslint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  extends: [
+    '@nuxt/eslint-config',
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.eslint.json',
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+  },
 };
