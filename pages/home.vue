754d66f120b4ce79f79b24f840e50c949ad494c3
Hugo <hmoralesg@icloud.com>
2024-02-09 03:29:57 -0300

    feat: added home page

---
+++ pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-100 h-100 flex-column d-flex align-center justify-center">
+    <p>Hi, this is the home page</p>
+    <v-btn @click="logout">
+      Logout
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { logout } = useLogout();
+
+definePageMeta({
+  title: 'Home',
+  middleware: ['auth'],
+  description: 'This is the home page',
+});
+</script>
