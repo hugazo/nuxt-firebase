2c5e8082cf26554f62cbbeb13c2092389e074571
Hugo <hmoralesg@icloud.com>
2024-02-14 01:26:59 -0300

    feat: first completed deployment to firebase

--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,4 +1,4 @@
-import { beforeUserCreated, beforeUserSignedIn } from 'firebase-functions/v2/identity';
+import { beforeUserCreated, beforeUserSignedIn, HttpsError } from 'firebase-functions/v2/identity';
 import { getAuth } from 'firebase-admin/auth';
 import { onCall } from 'firebase-functions/v2/https';
 import { logger } from 'firebase-functions/v2';
@@ -7,8 +7,12 @@ import { checkUserRole } from '../utils/auth.js';
 // Example of blocking functions
 // https://firebase.google.com/docs/auth/extend-with-blocking-functions?gen=2nd#understanding_blocking_functions
 
-export const logUserCreated = beforeUserCreated((event) => {
+export const handleUserCreationRules = beforeUserCreated((event) => {
   const user = event.data;
+  if (!user?.email?.includes('@42devs.cl')) {
+    logger.error('Unauthorized email', user.email);
+    throw new HttpsError('invalid-argument', 'Unauthorized email');
+  }
   logger.info('User Created', user);
 });
 
